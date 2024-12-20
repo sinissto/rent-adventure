7b032b3826f7f1df627620b43801563c50ac1a62
Sinisa <sinisha.stojanovic@gmail.com>
2024-12-20 15:54:30 +0100

    Supabase connected with the project and created queries to get motorbikes from DB

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -1,3 +1,5 @@
+import { supabase } from "@/app/_lib/supabase";
+
 export async function getCountries() {
   try {
     const res = await fetch(
@@ -8,4 +10,38 @@ export async function getCountries() {
   } catch {
     throw new Error("Could not fetch countries");
   }
+}
+
+/////////////
+// GET
+
+export const getBikes = async function () {
+  const { data, error } = await supabase
+    .from("motorbikes")
+    .select("id, brand, model, price, image")
+    .order("model");
+
+  if (error) {
+    console.error(error);
+    throw new Error("Motorbikes could not be loaded");
+  }
+
+  return data;
+};
+
+export async function getBike(id) {
+  const { data, error } = await supabase
+    .from("motorbikes")
+    .select("*")
+    .eq("id", id)
+    .single();
+
+  // For testing
+  // await new Promise((res) => setTimeout(res, 1000));
+
+  if (error) {
+    console.error(error);
+  }
+
+  return data;
 }
